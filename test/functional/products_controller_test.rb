807ec41802a09286feeed498ac53ed46db7295f5
sloppypig <miaoliucpe@gmail.com>
2012-03-03 15:59:32 -0700

    TaskC playtime added time on sidebar and functional test

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select "#product_list table", 1
+    assert_select ".list_line_even", 1
+    assert_select ".list_line_odd", 2
   end
 
   test "should get new" do
