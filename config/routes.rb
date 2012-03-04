fb6a8864082e9f22bb6e76b64e3b749abe7bb80f
sloppypig <miaoliucpe@gmail.com>
2012-03-03 17:41:11 -0700

    TaskD add cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Demo2::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
---
+++ db/migrate/20120304002433_update_line_item_column_name.rb
@@ -0,0 +1,5 @@
+class UpdateLineItemColumnName < ActiveRecord::Migration
+  def change
+    rename_column :line_items, :card_id, :cart_id
+  end
+end
